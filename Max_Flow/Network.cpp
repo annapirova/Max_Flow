9a55006011fb8bb02550081a0c45931ec392e5b7
Nesterov Alexander <nesoldr@mail.ru>
2016-01-02 18:03:56 +0300

    Changed class "Network" #2
    Added function

--- Max_Flow/Network.cpp
+++ Max_Flow/Network.cpp
@@ -2,11 +2,83 @@
 
 
 
-Network::Network()
+Network::Network(int size): net(size, size * size), list(size)
 {
+	size_net = size;
+
+	c = new double[size_net * size_net];
+	for (int i = 0; i < size_net * size_net; i++)
+		c[i] = 0;
+
+	h = new double[size_net];
+	e = new double[size_net];
+
+	f = new double*[size_net];
+	for (int i = 0; i < size_net; i++)
+	{
+		f[i] = new double[size_net];
+		for (int j = 0; j < size_net; j++)
+			f[i][j] = 0;
+		h[i] = 0; e[i] = 0;
+	}
 }
 
+Network::Network(const Graf_matrix &gr) :
+	net(gr), list(gr.Get_Size_M())
+{
+	size_net = gr.Get_Size_M;
+
+	c = new double[size_net * size_net];
+
+	int m = 0, k = 0;
+	for (int i = 0; i < size_net; i++)
+		for (int j = 0; j < size_net; j++)
+			if (gr[i][j] != 0)
+			{
+				c[m] = gr[i][j];
+				m++;
+				k++;
+			}
+
+	h = new double[size_net];
+	e = new double[size_net];
+
+	f = new double*[size_net];
+	for (int i = 0; i < size_net; i++)
+	{
+		f[i] = new double[size_net];
+		for (int j = 0; j < size_net; j++)
+			f[i][j] = 0;
+		h[i] = 0; e[i] = 0;
+	}
+}
+
+Network::Network(const Network &nt): 
+	size_net(nt.size_net), net(nt.net), list(nt.list)
+{
+	c = new double[size_net * size_net];
+	for (int i = 0; i < size_net * size_net; i++)
+		c[i] = nt.c[i];
+
+	h = new double[size_net];
+	e = new double[size_net];
+
+	f = new double*[size_net];
+	for (int i = 0; i < size_net; i++)
+	{
+		f[i] = new double[size_net];
+		for (int j = 0; j < size_net; j++)
+			f[i][j] = nt.f[i][j];
+		h[i] = nt.h[i]; e[i] = nt.e[i];
+	}
+}
 
 Network::~Network()
 {
+	delete[]c;
+	delete[]h;
+	delete[]e;
+	for (int i = 0; i < size_net; i++)
+		delete[]f[i];
+	delete[]f;
 }
--- Max_Flow/Network.h
+++ Max_Flow/Network.h
@@ -13,9 +13,16 @@ class Network
 	TQueue<int> list;
 	double *h;
 	double *e;
+	int size_net;
 
 public:
 	Network(int size = 5);
+	Network(const Graf_matrix &gr);
+	Network(const Network &nt);
+	double Get_flow(int u, int v) { return f[u][v]; };
+	void Set_flow(int u, int v, double val) { f[u][v] = val; };
+	int Get_New_Active_Vertex() { return list.Pop(); };
+	void Add_New_Active_Vertex(int vert) { list.Push(vert); };
 	~Network();
 };
 
